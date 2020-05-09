bc9b5d052f6ebf1bb310035dc62b7dda8ea20f20
Glioce <0>
2020-05-09 12:54:58 -0500

    agregado pulsos motor, pot y finales de carrera

--- proto01_01/proto01_01.ino
+++ proto01_01/proto01_01.ino
@@ -1,49 +1,117 @@
 /* RESPIRADOR ARTIFICIAL
- *  
- * Modelo 3D obtenido de Frax3D
- * 
- * El prototipo tiene un programa que controla la velocidad de desplazamiento del pistón,
- * pero es demasiado lento.
- * En esta versión del programa se incrementará la velocidad usando reduciendo la
- * tasa de actualización de la LCD (que requiere varios ms) y tal vez interrupciones y timers.
- * 
- * Se usa la biblioteca LiquidCrystal
 
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
+   Modelo 3D obtenido de Frax3D
+
+   El prototipo tiene un programa que controla la velocidad de desplazamiento del pistón,
+   pero es demasiado lento.
+   En esta versión del programa se incrementará la velocidad usando reduciendo la
+   tasa de actualización de la LCD (que requiere varios ms) y tal vez interrupciones y timers.
+
+   Se usa la biblioteca LiquidCrystal
+   LCD R/W pin to ground
+   LCD VSS pin to ground
+   LCD VCC pin to 5V
+   10K resistor:
+   ends to +5V and ground
+   wiper to LCD VO pin (pin 3)
 */
 
+// Fines de carrera, con resistencias pull-down, cuando se presionan cambian de 0 a 5 V
+#define PIN_SW1 6 //frontal
+#define PIN_SW2 7 //trasero
+//byte sw1; //estado actual del switch 1
+//byte sw1prev = 0; //estado previo
+//byte sw2; //estado actual del switch 2
+//byte sw2prev = 0; //estado previo
 
-#include <LiquidCrystal.h>
+
+// Motor a pasos con controlador A4988
+#define PIN_DIR 8
+#define PIN_STEP 9
 
 // Inicializar LCD
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+#include <LiquidCrystal.h>
+const int RS = 12, EN = 11, D4 = 5, D5 = 4, D6 = 3, D7 = 2;
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+#define T_LCD 500 //periodo de actualización de LCD
+unsigned long tlcd = 0; //momento de actualizar LCD
+unsigned long t;
+
+#define PIN_POT A0 //potenciómetro
+unsigned int pot; //lectura del pot
+unsigned int vel; //velocidad
 
 void setup() {
-  // set up the LCD's number of columns and rows:
+  // Usar 16 columnas y 2 filas
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
-  Serial.begin(9600);
+  // Mensaje inicial
+  lcd.setCursor(0, 0);
+  lcd.print("EXHALACION");
+  lcd.setCursor(0, 1);
+  //lcd.print("Potencia:");
+  lcd.print("Velocidad:");
+  //Serial.begin(9600);
+
+  pinMode(PIN_STEP, OUTPUT);
+  pinMode(PIN_DIR, OUTPUT);
+  digitalWrite(PIN_STEP, 0);
+  digitalWrite(PIN_DIR, 0);
+
+  pinMode(PIN_POT, INPUT);
+  analogRead(PIN_POT);
+
+  pinMode(PIN_SW1, INPUT);
+  pinMode(PIN_SW2, INPUT);
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
-  Serial.println(millis());
+  // Enviar pulso al conrolador del motor
+  digitalWrite(PIN_STEP, 1); //positivo
+  delayMicroseconds(100); //esperar
+  digitalWrite(PIN_STEP, 0); //regresar a cero
+
+  t = millis(); //leer momento actual
+  if (t >= tlcd) {// Si es momento de actualizar la LCD
+    tlcd += T_LCD; //agregar periodo de actualización
+    pot = analogRead(PIN_POT); //leer pot
+    vel = ((1023 - pot) * 3 / 31) + 1;
+    lcd.setCursor(11, 1); //posición para escribir
+    lcd.print(vel); //escribir valor
+    lcd.print("  "); //espacios para borrar carateres de la escritura anterior
+  }
+  else { //si no es momento de actualizar la LCD realizar un retardo
+    delay(1); //retardo mínimo
+    delayMicroseconds(pot*2); //retardo proporcional a la lectura del pot
+  }
+
+  // El motor ya avanzó un paso, checar fines de carrera
+  //frontal
+  if (digitalRead(PIN_SW1)) {//acaba de ser presionado
+    digitalWrite(PIN_DIR, 0);
+    lcd.setCursor(0, 0); //posición para escribir
+    lcd.print("EX"); //escribir valor
+  }
+  //trasero
+  if (digitalRead(PIN_SW2)) {//acaba de ser presionado
+    digitalWrite(PIN_DIR, 1);
+    lcd.setCursor(0, 0); //posición para escribir
+    lcd.print("IN"); //escribir valor
+  }
+  /*
+    sw1 = digitalRead(PIN_SW1); //frontal
+    if ((sw1 == 1) and (sw1prev = 0)) {//acaba de ser presionado
+    digitalWrite(PIN_DIR, 0);
+    lcd.setCursor(0, 0); //posición para escribir
+    lcd.print("EX"); //escribir valor
+    }
+    sw1prev = sw1; //actualizar estado previo
+
+    sw2 = digitalRead(PIN_SW2); //trasero
+    if ((sw2 == 1) and (sw2prev = 0)) {//acaba de ser presionado
+    digitalWrite(PIN_DIR, 1);
+    lcd.setCursor(0, 0); //posición para escribir
+    lcd.print("IN"); //escribir valor
+    }
+    sw2prev = sw2; //actualizar estado previo
+  */
 }
